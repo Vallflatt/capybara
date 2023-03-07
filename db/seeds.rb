134160f374f3752959bbaed5235a7ecc4a2bb0a2
Robin Goodyear <robingoodyear6@gmail.com>
2023-03-07 16:53:04 +0100

    user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+#Booking.destroy_all if Rails.env.development?
+#Article.destroy_all if Rails.env.development?
+User.destroy_all if Rails.env.development?
+puts "after destroy"
+
+puts "count user: #{User.count}"
+#puts "count booking: #{Booking.count}"
+#puts "count article: #{Article.count}"
+
+
+20.times do
+  User.create!(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: '123456'
+  )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts "count user: #{User.count}"
+#puts "count booking: #{Booking.count}"
+#puts "count article: #{Article.count}"
