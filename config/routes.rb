5a408c31ab6130b2161f5e36d0218355f28b261a
David de la Harpe <118104692+DDLH-92@users.noreply.github.com>
2023-03-08 22:43:12 +0100

    form-booking ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "articles/search", to: "articles#search"
   resources :articles, only: %i[index show create new] do
-    resources :bookings, only: %i[new create]
+    resources :bookings, only: %i[new create] do
+      get "confirm"
     end
+  end
   resources :bookings, only: %i[index destroy] do
   end
   # get "bookings/", to: "bookings#index"
-  # get "articles/id/bookings/new", to: "bookings#new"
-  # post "bookings/id", to:"bookings#create"
+  get "articles/id/bookings/new", to: "bookings#new"
+  post "bookings/id", to: "bookings#create"
 end
