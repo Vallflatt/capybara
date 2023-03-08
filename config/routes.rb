8c92e3b90dfbfdd3bd351c982df14c47476c1087
Virginie Allflatt <vallflatt@gmail.com>
2023-03-08 13:00:23 +0100

    WIP: create routes bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
   get "articles/search", to: "articles#search"
-
+  resources :articles, only: %i[index show create new] do
+    resources :bookings, only: %i[new create]
+    end
+  resources :bookings, only: %i[index destroy] do
+  end
+  # get "bookings/", to: "bookings#index"
+  # get "articles/id/bookings/new", to: "bookings#new"
+  # post "bookings/id", to:"bookings#create"
 end
